d162cfaad19da3d302bdd5747e3e49d93d1a796e
Lucas Machado <lukmail@gmail.com>
2014-05-23 15:12:19 -0300

    Now the description is not required

--- Model/Banner.php
+++ Model/Banner.php
@@ -59,16 +59,6 @@ class Banner extends BannerAppModel
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 		),
-		'description' => array(
-			'notEmpty' => array(
-				'rule' => array('notEmpty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
 		'link' => array(
 			'notEmpty' => array(
 				'rule' => array('notEmpty'),
